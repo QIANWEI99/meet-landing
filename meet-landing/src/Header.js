00295e2f31e6ea553bd60ec7e136e6f6030b3a25
KOK QIAN WEI <kokqianwei@KOKs-Mac-mini.local>
2022-08-19 17:15:03 +0800

    meet landing interface v2

--- meet-landing/src/Header.js
+++ meet-landing/src/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <Box>
             <Center my={['10','10','20','20','20','40']}>
-            Œ¸<svg 
+            <svg 
             width="119" 
             height="28" 
             xmlns="http://www.w3.org/2000/svg">
